25e33e2b42f619d443d02799ef8af74381c852d7
Mateusz Skalski <mat.skalski@gmail.com>
2019-03-17 23:33:02 +0100

    Add content script to manifest

--- src/background.js
+++ src/background.js
@@ -3,7 +3,13 @@
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.tabs.executeScript({
-        file: './display-panel/display-panel.js'
+        file: './display-panel/display-panel.js' // FIXME not working, maybe onInstalled isn't right hook
+    }, (result) => {
+        let e = chrome.runtime.lastError;
+        if (e !== undefined) { // in chrome's extensions tab
+            console.log(result);
+            return;
+        }
     });
 });
 
