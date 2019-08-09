7e7383962c3a2a2183eeb5ed2ed34b7ba7da5dcd
Mateusz Skalski <mat.skalski@gmail.com>
2019-08-09 14:41:06 +0200

    Add Too Much Going On benchmark

--- src/benchmarks/8-too-much-going-on/8-too-much-going-on.js
+++ src/benchmarks/8-too-much-going-on/8-too-much-going-on.js
@@ -1,3 +1,5 @@
+import {isVisible} from "../filters.js";
+
 class TooMuchGoingOn {
     getLabel = () => {
         return "8. Too much going on";
@@ -5,10 +7,11 @@ class TooMuchGoingOn {
 
     execute = () => {
         // Count headings
-        // Amount of content?
-        // Clutternes?
-        // Small margins between components?
-        return "NOT IMPLEMENTED";
+        let headings = [1,2,3,4,5,6].reduce((acc, i) => acc.concat([...document.getElementsByTagName(`h${i}`)]), []).filter(isVisible);
+        // TODO Amount of content?
+        // TODO Clutternes?
+        // TODO Small margins between components?
+        return `Headings ${headings.length}, PARTIALLY IMPLEMENTED`;
     }
 }
 
