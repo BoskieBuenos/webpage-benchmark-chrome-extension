e1d30a73db5f26d693b03cf7ca0b51ff9f6f861a
Mateusz Skalski <mat.skalski@gmail.com>
2019-08-13 16:47:06 +0200

    Add Inconsistent design - too many typefaces benchmark

--- src/benchmarks/10-too-confusing-unclear-hierarchy-inconsistency/10.1-inconsistent-design.js
+++ src/benchmarks/10-too-confusing-unclear-hierarchy-inconsistency/10.1-inconsistent-design.js
@@ -2,13 +2,24 @@
 /// Too many typefaces, color schemes, inconsistent design
 ///
 
+import {isVisible, distinct} from "../filters.js";
+
+function css(element, property) {
+    return window.getComputedStyle(element, null).getPropertyValue(property);
+}
+
 class InconsistentDesign {
     getLabel = () => {
         return "10.1 Too many typefaces, color schemes, inconsistent design";
     };
 
     execute = () => {
-        return "NOT IMPLEMENTED";
+        // TODO Detect actually rendered font. For now let's assume counting font families is sufficient.
+        let fontFamilies = [...document.getElementsByTagName('*')].filter(isVisible).map(element => css(element, 'font-family'));
+        let distinctFontFamilies = fontFamilies.filter(distinct);
+        // TODO Detect color schemes
+        // TODO Detect inconsistent design
+        return `Typefaces ${distinctFontFamilies.length} (${distinctFontFamilies.join(" / ")}), PARTIALLY IMPLEMENTED`;
     }
 }
 
