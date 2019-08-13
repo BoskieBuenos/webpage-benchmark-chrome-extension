caf69da4cc3cb9d8f6ab322a738975fb50500bdf
Mateusz Skalski <mat.skalski@gmail.com>
2019-08-13 22:39:39 +0200

    Add Confuding links benchmark

--- src/benchmarks/10-too-confusing-unclear-hierarchy-inconsistency/10.3-confusing-links.js
+++ src/benchmarks/10-too-confusing-unclear-hierarchy-inconsistency/10.3-confusing-links.js
@@ -2,13 +2,25 @@
 /// Similar links leading to different places
 ///
 
+import {isVisible, distinct} from "../filters.js";
+
 class ConfusingLinks {
     getLabel = () => {
         return "10.3 Similar links leading to different places";
     };
 
     execute = () => {
-        return "NOT IMPLEMENTED";
+        // Group links by similarity & verify their destinations
+        // TODO make fuzzy string comparison - for now exacts
+        let links = [...document.getElementsByTagName('a')].filter(isVisible);
+        let similarLabels = links.reduce((acc, link) => {
+            acc[link.innerText] = (acc[link.innerText] || []).concat(link);
+            return acc;
+        }, {});
+        let repeatingLabels = Object.keys(similarLabels).filter(k => similarLabels[k].length > 1).map(k => similarLabels[k]);
+        let repeatingLabelsLeadingToSimilarPlaces = repeatingLabels.map(l => l.filter(distinct)).filter(l => l.length > 1);
+        let isSimilarLinkLeadingToDifferentPlaces = repeatingLabelsLeadingToSimilarPlaces.length > 0;
+        return `${isSimilarLinkLeadingToDifferentPlaces ? 'YES' : 'NO'}`;
     }
 }
 
