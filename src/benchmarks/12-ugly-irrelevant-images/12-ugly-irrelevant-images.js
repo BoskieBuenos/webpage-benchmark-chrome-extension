7fa337c46b92dc0621fd78b318060e6c8d7c4220
Mateusz Skalski <mat.skalski@gmail.com>
2019-08-15 15:38:17 +0200

    Add Ugly image benchmark

--- src/benchmarks/12-ugly-irrelevant-images/12-ugly-irrelevant-images.js
+++ src/benchmarks/12-ugly-irrelevant-images/12-ugly-irrelevant-images.js
@@ -1,10 +1,18 @@
+import {css} from "../../utils/dom-utils.js";
+
 class UglyImages {
     getLabel = () => {
         return "12 Ugly or irrelevant images";
     };
 
     execute = () => {
-        return "NOT IMPLEMENTED";
+        // get all images and background-images
+        // compare their naturalHeight/Width with computed property - warn if streched too much
+        let imgs = [...document.getElementsByTagName('img')];
+        // TODO background-image-s
+        let backgroundImgs = [...document.getElementsByTagName('*')].filter(e => css(e, 'background-image'));
+        let streches = imgs.map(i => Math.max(parseInt(css(i, 'width'))/i.naturalWidth, parseInt(css(i, 'height'))/i.naturalHeight)).map(s => parseInt(s*100));
+        return `${streches.join("%, ")}%`;
     }
 }
 
