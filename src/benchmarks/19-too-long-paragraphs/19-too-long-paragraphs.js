a4eb01431fb009a44a1d05866e31eb50a79a2040
Mateusz Skalski <mat.skalski@gmail.com>
2019-08-15 22:03:38 +0200

    Add Paragraphs length benchmark

--- src/benchmarks/19-too-long-paragraphs/19-too-long-paragraphs.js
+++ src/benchmarks/19-too-long-paragraphs/19-too-long-paragraphs.js
@@ -1,10 +1,17 @@
+import {isVisible} from "../filters.js";
+
 class TooLongParagraphs {
     getLabel = () => {
         return "19 Too long paragraphs";
     };
 
     execute = () => {
-        return "NOT IMPLEMENTED";
+        let paragraphs = [...document.getElementsByTagName('p')].filter(isVisible).map(p => { return {
+            element: p,
+            words: p.innerText.split(' ').length
+        }});
+
+        return `lengths ${paragraphs.map(p => p.words).join()}`;
     }
 }
 
