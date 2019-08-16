032369a3a604b2f44ffe7b796cbb47b4f9322ad4
Mateusz Skalski <mat.skalski@gmail.com>
2019-08-16 14:55:25 +0200

    Add Small font benchmark

--- src/benchmarks/20-lack-of-legibility/20.1-small-font.js
+++ src/benchmarks/20-lack-of-legibility/20.1-small-font.js
@@ -1,10 +1,24 @@
+import {css} from "../../utils/dom-utils.js";
+
+const fontSizeThreshold = 100; // TODO select sensible value
+
 class SmallFont {
     getLabel = () => {
         return "20.1 Small font";
     };
 
     execute = () => {
-        return "NOT IMPLEMENTED";
+        // Get all elements with fonts below the threshold
+        let tooSmallElements = [...document.getElementsByTagName('*')].map(e => {
+            return {
+                element: e,
+                fontSize: parseFloat(css(e, 'font-size'))
+            }
+        }).filter(e => e.fontSize < fontSizeThreshold);
+
+        let minFontSize = Math.min(...tooSmallElements.map(e => e.fontSize));
+
+        return `elements below ${fontSizeThreshold}px ${tooSmallElements.length}, min font size ${minFontSize}px`;
     }
 }
 
