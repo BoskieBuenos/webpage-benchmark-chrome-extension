db1e64cec143ddf5ffa7f910d9af918c551476bc
Mateusz Skalski <mat.skalski@gmail.com>
2019-08-17 14:42:59 +0200

    Add no search bar benchmark]

--- src/benchmarks/29-no-search-bar/29-no-search-bar.js
+++ src/benchmarks/29-no-search-bar/29-no-search-bar.js
@@ -1,10 +1,20 @@
+// https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/Search_role
+
+import {attributeContains, notDisplayPanel, isVisible} from "../filters.js";
+
 class NoSearchBar {
     getLabel = () => {
         return "29 No search bar";
     };
 
     execute = () => {
-        return "NOT IMPLEMENTED";
+        // find form with role search
+        let searchForm = [...document.getElementsByTagName('form')]
+            .filter(attributeContains('role', 'search'))
+            .filter(notDisplayPanel)
+            .filter(isVisible);
+
+        return `search forms ${searchForm.length}`;
     }
 }
 
