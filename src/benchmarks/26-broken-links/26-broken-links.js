daaf22bf4fb0efc7b5cbef7d36fe1364d71ca15c
Mateusz Skalski <mat.skalski@gmail.com>
2019-08-18 15:07:09 +0200

    Add Broken links benchmark

--- src/benchmarks/26-broken-links/26-broken-links.js
+++ src/benchmarks/26-broken-links/26-broken-links.js
@@ -1,10 +1,42 @@
+import {isVisible, notDisplayPanel} from "../filters.js";
+
+const resultElementId = 'wbce-result-26';
+
 class BrokenLinks {
     getLabel = () => {
         return "26 Broken links";
     };
 
     execute = () => {
-        return "NOT IMPLEMENTED";
+        let allLinks = [...document.getElementsByTagName('a')]
+            .filter(notDisplayPanel)
+            .filter(isVisible)
+            .filter(link => link.href.length > 0);
+        let promises = Promise.all(allLinks.map(link => {
+            const url = link.href;
+            return new Promise((resolve, reject) => {
+                const xhr = new XMLHttpRequest();
+                xhr.open("GET", url);
+                xhr.onload = () => {
+                    if (xhr.status !== 404) {
+                        resolve(xhr.responseText)
+                    } else {
+                        reject(xhr.statusText);
+                    }
+                };
+                xhr.onerror = () => {
+                    reject(xhr.statusText);
+                };
+                xhr.send();
+            })
+        }));
+        promises.then((result) => {
+            document.getElementById(resultElementId).innerText = 'All links are OK'
+        });
+        promises.catch((result) => {
+            document.getElementById(resultElementId).innerText = 'Some links are broken'
+        });
+        return `<span id="${resultElementId}">Verifying links, it may take some time...</span>`;
     }
 }
 
