d918ff42c38b426d78927719311a9c5845cfd5b7
Mateusz Skalski <mat.skalski@gmail.com>
2019-08-18 16:43:26 +0200

    Add New window links benchmark

--- src/benchmarks/32-new-window-links/32-new-window-links.js
+++ src/benchmarks/32-new-window-links/32-new-window-links.js
@@ -1,10 +1,17 @@
+import {attributeContains, notDisplayPanel, isVisible} from "../filters.js";
+
 class NewWindowLinks {
     getLabel = () => {
         return "32 Links that open a new window";
     };
 
     execute = () => {
-        return "NOT IMPLEMENTED";
+        let newWindowLinks = [...document.getElementsByTagName('a')]
+            .filter(attributeContains('target', '_blank'))
+            .filter(notDisplayPanel)
+            .filter(isVisible);
+
+        return `${newWindowLinks.length}`;
     }
 }
 
