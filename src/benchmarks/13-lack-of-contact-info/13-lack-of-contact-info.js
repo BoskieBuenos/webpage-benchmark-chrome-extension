5fb382ea342d93e001a3f078e7a98a67255ff193
Mateusz Skalski <mat.skalski@gmail.com>
2019-08-19 19:18:25 +0200

    Add contackt page check

--- src/benchmarks/13-lack-of-contact-info/13-lack-of-contact-info.js
+++ src/benchmarks/13-lack-of-contact-info/13-lack-of-contact-info.js
@@ -1,4 +1,7 @@
 import {notDisplayPanel, isVisible, containsText, containsHeading} from "../filters.js";
+import {checkExistance} from "../../utils/ajax-utils.js";
+
+const contactPageResultElementId = 'wbce-result-13';
 
 class LackOfContactInfo {
     getLabel = () => {
@@ -7,10 +10,12 @@ class LackOfContactInfo {
 
     execute = () => {
         // Is there a form with "Contact" heading or "Contact" link or <main_url>/contact responds OK
+        // TODO it doesn't have to be form - it can contain just info
         let contactForm = [...document.getElementsByTagName('form')].filter(notDisplayPanel).filter(isVisible).filter(containsHeading('contact'));
         let contactLink = [...document.getElementsByTagName('a')].filter(notDisplayPanel).filter(isVisible).filter(containsText('contact'));
-        // TODO <main_url>/contact responds OK
-        return `Contact forms ${contactForm.length}, contact links ${contactLink.length}`;
+        let promise = checkExistance(`${window.location.origin}/contact`);
+        promise.then((r) => document.getElementById(contactPageResultElementId).innerText = 'YES');
+        return `Contact forms ${contactForm.length}, contact links ${contactLink.length}, contact page <span id="${contactPageResultElementId}">Missing</span>`;
     }
 }
 
